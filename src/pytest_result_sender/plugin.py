b639b6d963dcea909f7e14e3ac8966a0c36198e6
NBDatsuya <2646809050@qq.com>
2024-04-15 09:40:00 +0800

    fix: plugin.py: pytest_runtest_logreport, pytest_collection_finish

--- src/pytest_result_sender/plugin.py
+++ src/pytest_result_sender/plugin.py
@@ -1,20 +1,47 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+
 import pytest
 
-data = {}
+data = {
+    "failed": 0,
+    "passed": 0,
+    "start_time": datetime.now(),
+    "end_time": datetime.now(),
+    "pass_ratio": 0.0,
+    "pass_ratio_percentage": "0.000000%"
+}
 
 
-def pytest_configure():
+def pytest_configure(config):
     # exq after conf loading and be4 tuc exq
-    data['start_time'] = datetime.now()
+    data["start_time"] = datetime.now()
     print(f"{data['start_time']} python开始执行")
 
 
-def pytest_unconfigure():
+def pytest_unconfigure(config):
     # exq b4 conf unloading and be4 tuc exq
-    data['end_time'] = datetime.now()
+    data["end_time"] = datetime.now()
     print(f"{data['end_time']} python结束执行")
 
-    data['duration'] = data['end_time'] - data['start_time']
+    data["duration"] = data["end_time"] - data["start_time"]
+    data["pass_ratio"] = data["passed"] / data["total"]
+    data["pass_ratio_percentage"] = "%.6f%%" % (data["pass_ratio"]*100)
 
     print(data)
+
+    assert timedelta(seconds=3.5) > data["duration"] >= timedelta(seconds=3.1)
+    assert data["total"] == 3
+    assert data["passed"] == 2
+    assert data["failed"] == 1
+    assert data["pass_ratio"] == 2 / 3
+    assert data["pass_ratio_percentage"] == "66.6667%"
+
+
+def pytest_collection_finish(session: pytest.Session):
+    data["total"] = len(session.items)
+
+
+def pytest_runtest_logreport(report: pytest.TestReport):
+    # setup call teardown
+    if report.when == "call":
+        data[report.outcome] += 1
--- tests/test_abc.py
+++ tests/test_abc.py
@@ -3,3 +3,11 @@ import time
 
 def test_1():
     time.sleep(3.2)
+
+
+def test_pass():
+    pass
+
+
+def test_fail():
+    assert False
